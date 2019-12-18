09b0dfc42f9fbd90c6f4315f7ddb621bd684ea6c
thomas6886 <thomas.6886@hotmail.com>
2019-12-18 16:33:08 +0100

    Basic REST API in place

--- models/wedstrijd.js
+++ models/wedstrijd.js
@@ -1,5 +1,34 @@
 const mongoose = require('mongoose');
 
 const wedstrijdSchema = new mongoose.Schema({
-    
+    poule: {
+        type: String,
+        required: true
+    },
+    teamThuis: {
+        type: String,
+        required: true
+    },
+    teamGast: {
+        type: String,
+        required: true
+    },
+    puntenThuis: {
+        type: Number,
+        required: true
+    },
+    puntenGast: {
+        type: Number,
+        required: true
+    },
+    startTijd: {
+        type: Date,
+        required: true
+    },
+    veld: {
+        type: String,
+        required: true
+    }
 });
+
+module.exports = mongoose.model('Wedstrijd', wedstrijdSchema);
--- routes/wedstrijden.js
+++ routes/wedstrijden.js
@@ -1,25 +1,81 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
+const Wedstrijd = require('../models/wedstrijd');
 
-// Get all subscribers
-router.get('/', (req, res) => {
-    res.send('Hello World')
-})
+// Get all wedstrijden
+router.get('/', async (req, res) => {
+    try {
+        const wedstrijden = await Wedstrijd.find();
+        res.json(wedstrijden);
+    } catch (err) {
+        res.status(500).json({ message: err.message })
+    }
+});
 
-// Get one subscriber
-router.get('/:id', (req, res) => {
-})
+// Get one wedstrijd
+router.get('/:id', getWedstrijd, (req, res) => {
+    res.json(res.wedstrijd);
+});
 
-// Create one subscriber
-router.post('/', (req, res) => {
-})
+// Create one wedstrijd
+router.post('/', async (req, res) => {
+    const wedstrijd = new Wedstrijd({
+        poule: req.body.poule,
+        teamThuis: req.body.teamThuis,
+        teamGast: req.body.teamGast,
+        puntenThuis: req.body.puntenThuis,
+        puntenGast: req.body.puntenGast,
+        startTijd: req.body.startTijd,
+        veld: req.body.veld
+    });
 
-// Update one subscriber
-router.patch('/:id', (req, res) => {
-})
+    try {
+        const newWedstrijd = await wedstrijd.save()
+        res.status(201).json(newWedstrijd)
+    } catch (err) {
+        res.status(400).json({ message: err.message })
+    }
+});
 
-// Delete one subscriber
-router.delete('/:id', (req, res) => {
-})
+// Update one wedstrijd
+router.patch('/:id', getWedstrijd, async(req, res) => {
+    if (req.body.puntenThuis != null) {
+        res.wedstrijd.puntenThuis = req.body.puntenThuis
+    }
 
-module.exports = router
+    if (req.body.puntenGast != null) {
+        res.wedstrijd.puntenGast = req.body.puntenGast
+    }
+    try {
+        const updatedWedstrijd = await res.wedstrijd.save();
+        res.json(updatedWedstrijd);
+    } catch (err){
+        res.status(400).json({ message: err.message })
+    }
+});
+
+// Delete one wedstrijd
+router.delete('/:id', getWedstrijd, async (req, res) => {
+    try {
+        await res.wedstrijd.remove()
+        res.json({ message: 'Deze wedstrijd is verwijderd' })
+    } catch(err) {
+        res.status(500).json({ message: err.message })
+    }
+});
+
+async function getWedstrijd(req, res, next) {
+    try {
+        wedstrijd = await Wedstrijd.findById(req.params.id);
+        if (wedstrijd == null) {
+            return res.status(404).json({ message: 'Deze wedstrijd bestaat niet'})
+        }
+    } catch(err){
+        return res.status(500).json({ message: err.message })
+    }
+
+    res.wedstrijd = wedstrijd;
+    next()
+}
+
+module.exports = router;
