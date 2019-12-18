7b1a297c50be3f5cb1288c34b9c306c6166f3c96
thomas6886 <thomas.6886@hotmail.com>
2019-12-18 15:21:17 +0100

    Set-up standard REST stuff

---
+++ models/wedstrijd.js
@@ -0,0 +1,5 @@
+const mongoose = require('mongoose');
+
+const wedstrijdSchema = new mongoose.Schema({
+    
+});
---
+++ server.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+
+//Database connection
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+const db = mongoose.connection;
+db.on('error', (error) => console.error(error));
+db.once('open', () => console.log('connected to database'));
+
+//Make server handle JSON properly
+app.use(express.json());
+
+//Routes
+const wedstrijdenRouter = require('./routes/wedstrijden');
+app.use('/wedstrijden', wedstrijdenRouter);
+
+
+//Declare app started
+app.listen(3000, () => console.log('server started'));
